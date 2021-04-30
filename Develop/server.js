9ea5298f493201cf339cb8ef8d9f559366d7ac98
Zachary <taylor67469@yahoo.com>
2021-04-29 23:23:05 -0500

    im stuck >:(

--- Develop/server.js
+++ Develop/server.js
@@ -17,19 +17,26 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'db/db.json')));
-app.get('/api/notes',(req,res)=>{
+// app.get('/api/notes',(req,res)=>{
     
-    const chosen=req.params.newNote;
-    console.log(chosen);
-    for (let i=0;i<notes.length;i++)
-    return res.json(notes[i])
-})
+//     fs.readFile('db.json', res , (err, data) => {
+//         if (err) {
+//           console.error(err)
+//           return
+//         }
+//         console.log(data)
+//       })
+// })
 app.post('/api/notes',(req,res)=>{
-    const note=req.body;
-    note.routeName=note.title;
-    console.log(notes);
-
-    notes.push(note);
-    res.json(note);
+    notes.push(req.body);
+    for (let i = 0;i<notes.length;i++)
+    res.json();
+    fs.appendFile('db.json',notes[i].title,err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+    // return notes[i]
+})
 })
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
