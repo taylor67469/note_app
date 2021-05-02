d11562d6b7f9cc7734c0cae927bd56380049e2ac
Zachary <taylor67469@yahoo.com>
2021-05-01 23:05:56 -0500

    work please

--- Develop/server.js
+++ Develop/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')));
