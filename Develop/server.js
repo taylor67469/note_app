92fdae4e406f4a60755c2f146ac44fb19840874b
Zachary <taylor67469@yahoo.com>
2021-05-01 22:44:38 -0500

    fixing heroku hopefully

--- Develop/server.js
+++ Develop/server.js
@@ -16,9 +16,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
-app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'db/db.json')));
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')));
 app.post(`/api/notes`, (req, res) => {
   const newNote = req.body;
   newNote.id = uuidv1();
